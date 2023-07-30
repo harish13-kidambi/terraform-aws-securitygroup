99bbd01fca7d39546e610b320a059db277dccee9
Harish Kidambi <harish.kidambi@gmail.com>
2023-07-30 16:54:41 +0530

    updated

--- variables.tf
+++ variables.tf
@@ -19,5 +19,20 @@ variable "security_group_tags" {
 #optional
 variable "ingress" {
   type = map
-  default = {}
+  default = {
+    ingress-1 = {
+      from_port = 5432
+      to_port = 5432
+      protocol = "tcp"
+      description = "Allow 5432 port number"
+      cidr_block = ["0.0.0.0"]
+    },
+    ingress-2 = {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      description = "Allow port number from internet"
+      cidr_block = ["0.0.0.0"]
+    }
+  }
 }
